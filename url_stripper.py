539316843845efc7c5f7a0a1beef8cd530390723
Jacob Corum <cor15031@byui.edu>
2019-11-19 17:27:14 -0700

    rearranged files to have a seperate .ipynb for each dataset so that it is more organized. also created a function folder for all the functions that i create

--- url_stripper.py
+++
@@ -1,6 +0,0 @@
-def url_stripper(first_column, second_column, replacement): 
-    first_column = first_column.astype('str')
-    second_column = deck_type_urls.url.str.extract(replacement)
-    second_column = second_column.str.replace('>', '')
-    second_column = second_column.str.replace('<', '')
-    second_column = second_column.str.strip()
