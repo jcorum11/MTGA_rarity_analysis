5ac3c30f9dafa1390cb59d0374f4e2927e9e34c7
Jacob Corum <cor15031@byui.edu>
2019-12-07 17:09:01 -0700

    added the rest of the .ipynb files to mtg_scraper.py. also added some comments to f_decks_clean.ipynb

--- scraper/mtg_scraper.py
+++ scraper/mtg_scraper.py
@@ -8,6 +8,7 @@ import re
 import os
 import timeit
 import filecmp
+import url_stripper
 
 # pull deck type urls from mtgtop8.com with requests package
 url = 'https://www.mtgtop8.com/format?f=ST'
@@ -71,3 +72,57 @@ with open('C:/Users/muroc/Documents/MTG/type_html_files/4-5c_Aggro.html') as fil
     soup = BeautifulSoup(file, 'lxml')
 for a in soup.find_all(href=re.compile(r'search\?player')):
     players.append(a)
+
+# get a list of all urls in all files
+players = []
+urls = []
+
+for i in np.arange(np.count_nonzero(dir_names)):
+    with open('C:/Users/muroc/Documents/MTG/type_html_files/' + dir_names[i]) as file:
+        soup = BeautifulSoup(file, 'lxml')
+    for a in soup.find_all(href=re.compile(r'search\?player')):
+        players.append(a)
+    for a in soup.find_all(href=re.compile(r'event\?e\=.+\&d\=.+\&f\=ST')):
+        urls.append(a) 
+
+# put players and urls into a dataframe
+deck_urls = pd.DataFrame({'player': players, 
+                         'url': urls})
+
+# get deck names and clean them up
+url_stripper.url_stripper(deck_urls, 'url', 'name', '(>.+<)')
+
+# extract url from the url column
+deck_urls['url'] = deck_urls.url.str.extract('(event.+f\=ST)')
+
+# add root url to the data
+deck_urls.url = 'https://www.mtgtop8.com/' + deck_urls.url
+
+# clean urls and name
+deck_urls['name'] = deck_urls.name.str.replace(' ', '_')
+deck_urls['name'] = deck_urls.name.str.replace('/', '-')
+url_stripper.url_stripper(deck_urls, 'player', 'player', '(>.+<)')
+deck_urls.player = deck_urls.player.str.replace(' ', '_')
+
+# create a web crawler to request html pages from deck_type_urls
+# create a new path and name it newpath
+newpath = 'C:/Users/muroc/Documents/MTG/html_files'
+
+# check to make sure that there is no path that matches newpath and create a folder called html_files if there isn't
+if not os.path.exists(newpath):
+    os.makedirs(newpath)
+
+# change the current directory to newpath
+os.chdir(newpath)
+
+# scrape all html files from urls in deck_urls and put them in html_files folder
+deck_errors = []
+
+for i in np.arange(deck_urls.shape[0]):
+    try:
+        deck_html = requests.get(deck_urls.url[i])
+        with open(str(deck_urls.player[i]) + '_' + str(deck_urls.name[i]) + '.html', 'wb') as file:
+            file.write(deck_html.content)
+    except Exception as e:
+        print(str(i) + ' ' + str(e))
+        deck_errors.append(i)
