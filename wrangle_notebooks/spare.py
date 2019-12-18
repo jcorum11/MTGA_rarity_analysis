c848dbe27a1100137728b5e9d5bcfb77e697a137
Jacob Corum <cor15031@byui.edu>
2019-12-17 17:59:19 -0700

    attempting to figure out how to split numbers from card names

---
+++ wrangle_notebooks/spare.py
@@ -0,0 +1,17 @@
+import pandas as pd 
+import numpy as np
+
+decks_clean = pd.read_csv('C:/Users/muroc/Documents/MTG/data/decks_clean.csv')
+
+card_lists = []
+for index, rows in decks_clean.iterrows():
+    li = [rows.deck_list]
+    card_lists.append(li)
+
+cards = []
+for li in card_lists:
+    for a in li: 
+        for card in a:
+            cards.append(card)
+
+print(cards)
