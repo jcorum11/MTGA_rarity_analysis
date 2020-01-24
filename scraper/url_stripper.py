04b15f8855d9143c3d377432077940d66232b255
Jacob Corum <cor15031@byui.edu>
2020-01-24 08:20:51 -0700

    cleaned up the directory and added lines that delete imported files to scrapers. also modified directory references in mtg_scraper.ipynb.

--- scraper/url_stripper.py
+++
@@ -1,8 +0,0 @@
-def url_stripper(data, old_column, new_column_name, replacement): 
-    data[old_column] = data[old_column].astype('str')
-    data[new_column_name] = data[old_column].str.extract(replacement)
-    data[new_column_name] = data[new_column_name].str.replace('>', '')
-    data[new_column_name] = data[new_column_name].str.replace('<', '')
-    data[new_column_name] = data[new_column_name].str.strip()
-
-
